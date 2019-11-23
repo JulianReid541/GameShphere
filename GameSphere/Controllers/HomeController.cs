66b597f07a90464454b63d74acfc691bfeb5edfa
JulianReid541 <reidjulian@outlook.com>
2019-11-22 19:30:07 -0800

    added more functionality

--- GameSphere/Controllers/HomeController.cs
+++ GameSphere/Controllers/HomeController.cs
@@ -10,18 +10,46 @@ namespace GameSphere.Controllers
 {
     public class HomeController : Controller
     {
+        //TODO make post model add post lists to user
         //testingdata
         #region
-        User test1; 
+        User test1;
+        User test2;
+        Post p;
         public HomeController()
         {
             if(Repository.Users.Count == 0)
             {
                 test1 = new User()
                 {
-                    UserName = "test"
+                    UserName = "test",
+                    Game = "Call of Duty",
+                    Console = "Xbox",
+                    Genre = "FPS",
+                    Platform = "Twitch",
+                    Privacy = true
                 };
                 Repository.Users.Add(test1);
+                test2 = new User()
+                {
+                    UserName = "test2",
+                    Game = "Halo 4",
+                    Console = "PC",
+                    Genre = "Horror",
+                    Platform = "YoutubeGaming",
+                    Privacy = false                  
+                };
+                Repository.Users.Add(test2);
+                p = new Post()
+                {
+                    User = test2,
+                    Message = "This new site is amazing"
+                };
+                test2.AddPost(p);
+                test1.AddFollowing(test2);
+                test1.AddFollower(test2);
+                test2.AddFollower(test1);
+                test2.AddFollowing(test1);
             }
         }
         #endregion
@@ -45,31 +73,48 @@ namespace GameSphere.Controllers
 
         public ActionResult HomePage(User user)
         {
+            //TODO create wall displaying USER's Posts and following posts chronologically
+            ViewBag.postCount = user.Posts.Count;
+            ViewBag.followingCount = user.Following.Count;
+            ViewBag.followerCount = user.Followers.Count;
             return View(user);
         }
 
         [HttpGet]
         public IActionResult SignUp()
-        {
-            //TO DO finish questions store with username
+        {          
             return View();
         }
 
         [HttpPost]
-        public RedirectToActionResult SignUp(string username)
+        public RedirectToActionResult SignUp(string username, string game, string console,
+                                             string genre, string platform, bool privacy)
         {
             User user = new User();
             user.UserName = username;
+            user.Game = game;
+            user.Console = console;
+            user.Genre = genre;
+            user.Platform = platform;
+            user.Privacy = privacy;
             Repository.Users.Add(user);
             return RedirectToAction("Index");
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Privacy(string title)
         {
             //TODO create setting check to allow quiz results to be saved and shown or not
-            return View();
+            return View("Privacy", title);
         }
 
+        [HttpPost]
+        public RedirectToActionResult Privacy(string title, bool privacy)
+        {
+            User user = Repository.GetUserByUserName(title);
+            user.changeUserPrivacy(privacy);
+            return RedirectToAction("Homepage", user);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- GameSphere/Models/User.cs
+++ GameSphere/Models/User.cs
@@ -9,10 +9,45 @@ namespace GameSphere.Models
     {
         private List<User> followers = new List<User>();
         private List<User> following = new List<User>();
+        private List<Post> posts = new List<Post>();
 
         public string UserName { get; set; }
+        public string Game { get; set; }
+        public string Console { get; set; }
+        public string Genre { get; set; }
+        public string Platform { get; set; }
+        public bool Privacy { get; set; }
 
         public List<User> Followers { get { return followers; } }
         public List<User> Following { get { return following; } }
+        public List<Post> Posts { get { return posts; } }
+
+        public void AddFollowing(User user)
+        {
+            Following.Add(user);
+        }
+
+        public void AddFollower(User user)
+        {
+            Followers.Add(user);
+        }
+
+        public void AddPost(Post p)
+        {
+            Posts.Add(p);
+        }
+
+        public bool changeUserPrivacy(bool tf)
+        {
+            if (tf == Privacy)
+            {
+                return Privacy;
+            }
+            else
+            {
+                Privacy = tf;
+                return Privacy;
+            }
+        }
     }
 }
