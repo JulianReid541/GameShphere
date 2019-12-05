278b29b6ab6b23955e495bca112f1d38019c67a3
JulianReid541 <reidjulian@outlook.com>
2019-12-05 14:09:13 -0800

    had to add transients to make website use default repo

--- GameSphere/Startup.cs
+++ GameSphere/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using GameSphere.Models;
 
 namespace GameSphere
 {
@@ -33,6 +34,8 @@ namespace GameSphere
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<IRepository, Repository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
