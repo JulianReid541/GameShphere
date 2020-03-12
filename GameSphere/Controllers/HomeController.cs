c9604049a5b30eb2ce600558c6408527f7686ff4
JulianReid541 <reidjulian@outlook.com>
2020-03-12 12:13:57 -0700

    final commit

--- GameSphere/Controllers/HomeController.cs
+++ GameSphere/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GameSphere.Models;
-using System.Web;
-using System.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
